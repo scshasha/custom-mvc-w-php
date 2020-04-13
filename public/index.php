4c1f2286aeb3c25e4fdf982cbb2459ca3e7700c5
Siv Shasha <shasha.christopher@gmail.com>
2020-04-13 04:28:35 +0200

    fixed composer fils & correctly using autloader

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: *');
-// header('Access-Control-Allow-Headers: *');
-// header('Access-Control-Allow-Credentials: true');
 
-require_once "../src/init.php";
+// require_once "../src/init.php";
+require_once "../vendor/autoload.php";
 
-//var_dump(DEFAULT_CONTROLLER,DEFAULT_CONTROLLER_ACTION);die();
 $App = new App\Core\App;
 $App->run();
